154ca227d3ae577cd74bc04c5d49207f944ffa00
Craig Stuart Sapp <craigsapp@gmail.com>
2018-06-19 17:10:19 -0700

    Add midifile library code.

---
+++ tools/midi2exp.cpp
@@ -0,0 +1,21 @@
+
+//
+// Programmer:    Kitty Shi
+// Creation Date: Tue Jun 19 16:15:12 PDT 2018
+// Last Modified: Tue Jun 19 16:15:16 PDT 2018
+// Filename:      midi2exp/tools/midi2exp.cpp
+// Website:       https://github.com/pianoroll/midi2exp
+// Syntax:        C++11
+// vim:           ts=3 noexpandtab
+//
+// description:   Interpret expression for piano roll MIDI files.
+//
+
+
+int main(int argc, char** argv) {
+
+
+	return 0;
+}
+
+

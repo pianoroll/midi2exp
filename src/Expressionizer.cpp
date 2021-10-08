ac7e448ab9b7f48a3c7855999d05f58b708c04c3
Zhengshan Shi <kittyzhengshanshi@gmail.com>
2021-10-08 14:35:05 -0700

    duo-art mapping

--- src/Expressionizer.cpp
+++ src/Expressionizer.cpp
@@ -1768,9 +1768,10 @@ void Expressionizer::calculateDuoArtExpression(const std::string &option) {
         } else {
             pressure->at(i) = step2pressure(step->at(i), "right");
         }
-        // map pressure level to MIDI velocity, 5-33 to 38-80
-        expression_list->at(i) = (pressure->at(i)-5.0)/28.0*42.0 + 38;
-        cout << expression_list->at(i) << endl;
+        // map pressure level to MIDI velocity, 5-33 to 38-95
+        expression_list->at(i) = (pressure->at(i)-5.0)/28.0*57.0 + 38;
+        //cout << expression_list->at(i) << endl;
+        //cout << step->at(i) << "--" << pressure->at(i) << "--" << expression_list->at(i) << endl;
         if (isFastC->at(i)) {
             expression_list->at(i) = snake_f;   // 95 for snakebite velocity
         }
